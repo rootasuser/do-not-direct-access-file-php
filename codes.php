233110c4f9fe472bee48adf146512f156ab51610
Windel Navales <126387763+rootasuser@users.noreply.github.com>
2025-08-26 13:02:24 +0800

    Update codes.php

--- codes.php
+++ codes.php
@@ -1,6 +1,9 @@
+<?php
 if (basename($_SERVER['PHP_SELF']) == 'tracker.php') {
 	//echo basename($_SERVER['PHP_SELF']);
     require "../fileNotFound.php";
     exit();
     //die('Direct access not allowed.');
 }
+
+?>
